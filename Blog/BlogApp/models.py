803b0a071c5705e19dbb6687d3883152c9f94501
NadaBayoumy <nada.bayoumy@hotmail.com>
2017-02-09 20:41:28 +0200

    update img url

--- Blog/BlogApp/models.py
+++ Blog/BlogApp/models.py
@@ -12,7 +12,7 @@ class Category (models.Model):
 
 class Post (models.Model):
     postTitle = models.CharField(max_length = 180)
-    postPic = models.ImageField(verbose_name = "Post Image", upload_to = "static/img")
+    postPic = models.ImageField(verbose_name = "Post Image")
     postContent = models.TextField()
     userID = models.ForeignKey(User , related_name = 'users')
     postDate = models.DateTimeField(auto_now_add = True)
